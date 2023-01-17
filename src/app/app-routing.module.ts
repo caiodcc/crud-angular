f2fdc8201496d2cd8a098b8cdf97f61c2ef20a07
caiodcc <caio.dcc.marques@gmail.com>
2023-01-17 18:30:28 -0300

    Adicionando componentes!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterScreenComponent } from './components/register-screen/register-screen.component';
+import { LoginScreenComponent } from './components/login-screen/login-screen.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'navbar-logged', component: NavbarComponent},
+  {path: 'login-screen', component: LoginScreenComponent},
+  {path:'register-screen', component: RegisterScreenComponent},
+  {path:'', redirectTo: 'login-screen', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginScreenComponent } from './components/login-screen/login-screen.component';
+import { RegisterScreenComponent } from './components/register-screen/register-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginScreenComponent,
+    RegisterScreenComponent
   ],
   imports: [
     BrowserModule,
