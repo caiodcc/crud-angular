cff5e078890ac7f4806feb4bcfe273df2f0ba86c
caiodcc <caio.dcc.marques@gmail.com>
2023-01-20 18:04:36 -0300

    Configurações Bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RegisterScreenComponent } from './components/register-screen/register-s
 import { LoginScreenComponent } from './components/login-screen/login-screen.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 const routes: Routes = [
   {path: 'navbar-logged', component: NavbarComponent},
   {path: 'login-screen', component: LoginScreenComponent},
   {path:'register-screen', component: RegisterScreenComponent},
+  {path:'app-footer', component: FooterComponent},
   {path:'', redirectTo: 'login-screen', pathMatch: 'full'}
 
 ];
